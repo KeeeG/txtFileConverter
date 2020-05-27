64c0d71e375a1e89588ff66348a564c504b78f04
KeeeG <KeeeG>
2020-05-28 00:43:50 +0200

    WIP: reading a text file

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,16 @@
+import java.io.File;
+import java.util.Scanner;
+
 public class Main {
 
-        public static void main(String[] args) {
+        public static void main(String[] args) throws Exception {
 
-            System.out.println("Hello, World!");
-
+            File file = new File("/Users/keeg/workspace/txtFileConverter/src/main/resources/input.txt");
+            Scanner sc = new Scanner(file);
+            while (sc.hasNextLine())
+                System.out.println(sc.nextLine());
         }
 
 }
+
+
