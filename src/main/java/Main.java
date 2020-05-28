fc75f0c2b32eebcd689d6369425f87d318eaf056
KeeeG <KeeeG>
2020-05-28 16:50:37 +0200

    WIP: Cleaning

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,88 +6,107 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 
 public class Main {
 
+    final static String[] types = new String[] { "R", "G", "B" };
+    final static List<String> typesList = Arrays.asList(types);
+
         public static void main(String[] args) throws Exception {
 
-            String[] types = new String[] { "R", "G", "B" };
-            List<String> typesList = Arrays.asList(types);
+            Map<String, List<List<String>>> fileContent = new HashMap<>();
 
-            File file = new File( args[0] );
-            Scanner sc = new Scanner(file);
-
-
-            if (  args[1] != null && args[1].toString().toUpperCase().equals("JSON")){
-
+            // Checking if first argument ( input file path is not null
+            if (args[0]!= null){
+                fileContent = parseTextFile(args[0]);
                 JSONObject results = new JSONObject();
 
-                List<JSONObject> healthyLine  = new ArrayList();
-                List<JSONObject> unhealthyLine  = new ArrayList();
-                List<String> line  = new ArrayList();
+                // Checking if the output will be JSON or not
+                if (  args[1] != null && args[1].toString().toUpperCase().equals("JSON")){
+                    results = buildJSON(fileContent, args[0], args[2]);
 
-                while (sc.hasNextLine()){
-                    line = Arrays.asList(sc.nextLine().split(";"));
-                    JSONObject obj = new JSONObject();
-                    obj.put("numReference", line.get(0));
-                    obj.put("type", line.get(1));
-                    obj.put("price", line.get(2));
-                    obj.put("size", line.get(3));
+                    try (FileWriter filer = new FileWriter( args[2] )) {
 
-                    if ( !typesList.contains(line.get(1)) || (line.get(0).length() != 10) ){
-                        unhealthyLine.add(obj);
-                    }
-                    else {
-                        healthyLine.add(obj);
+                        filer.write(results.toString());
+                        filer.flush();
+
+                    } catch (IOException e) {
+                        e.printStackTrace();
                     }
                 }
-
-                results.put("inputFile" ,  args[0]);
-                results.put("references" , healthyLine);
-                results.put("errors" , unhealthyLine);
-
-                try (FileWriter filer = new FileWriter( args[2] )) {
-
-                    filer.write(results.toString());
-                    filer.flush();
-
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-
-            } else if (args[1].toString().toUpperCase().equals("XML")){
-               // String xml = XML.toString(results);
-                System.out.println("XML");
             }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
         }
+
+
+        private static JSONObject buildJSON (Map<String, List<List<String>>> file, String inputPath, String outputPath)throws Exception{
+
+            JSONObject results = new JSONObject();
+
+            results.put("inputFile", inputPath);
+            List<JSONObject> healthyLine  = new ArrayList();
+            List<JSONObject> unhealthyLine  = new ArrayList();
+
+            for (int i = 0; i < file.get("references").size(); i++){
+
+                JSONObject obj = new JSONObject();
+
+                obj.put("numReference", file.get("references").get(i).get(0));
+                obj.put("type", file.get("references").get(i).get(1));
+                obj.put("price", file.get("references").get(i).get(2));
+                obj.put("size", file.get("references").get(i).get(3));
+
+                healthyLine.add(obj);
+
+            }
+            results.put("references" , healthyLine);
+
+            for (int i = 0; i < file.get("errors").size(); i++){
+
+                JSONObject obj = new JSONObject();
+
+                obj.put("numReference", file.get("errors").get(i).get(0));
+                obj.put("type", file.get("errors").get(i).get(1));
+                obj.put("price", file.get("errors").get(i).get(2));
+                obj.put("size", file.get("errors").get(i).get(3));
+
+                unhealthyLine.add(obj);
+            }
+            results.put("errors" , unhealthyLine);
+
+            return results;
+        }
+
+        private static Map<String, List<List<String>>> parseTextFile (String inputFile) throws Exception{
+
+            Map<String, List<List<String>>> results = new HashMap<>();
+            List<List<String>> healthyLines = new ArrayList<>();
+            List<List<String>> unhealthyLines = new ArrayList<>();
+
+            if ( ! inputFile.isEmpty()) {
+
+                File file = new File( inputFile );
+                Scanner sc = new Scanner(file);
+
+                while (sc.hasNextLine()){
+                    List<String> line = new ArrayList<>();
+                    line = Arrays.asList(sc.nextLine().split(";"));
+
+                    if ( !typesList.contains(line.get(1)) || (line.get(0).length() != 10) ){
+                        unhealthyLines.add(line);
+                    }
+                    else {
+                        healthyLines.add(line);
+                    }
+                }
+                results.put("references", healthyLines);
+                results.put("errors", unhealthyLines);
+            }
+            return results;
+        }
+
 }
 
 
