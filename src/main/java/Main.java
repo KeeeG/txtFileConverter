0ad870019b764de5835a0ae5181ef5dd9b245880
KeeeG <KeeeG>
2020-05-28 15:40:14 +0200

    WIP: Refactoring 1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,5 @@
 import org.json.JSONObject;
+import org.json.XML;
 
 import java.io.File;
 import java.io.FileWriter;
@@ -16,47 +17,75 @@ public class Main {
         public static void main(String[] args) throws Exception {
 
             String[] types = new String[] { "R", "G", "B" };
-
-            // Convert String Array to List
             List<String> typesList = Arrays.asList(types);
-            JSONObject results = new JSONObject();
 
-            List<JSONObject> healthyLine  = new ArrayList();
-            List<JSONObject> unhealthyLine  = new ArrayList();
-            List<String> line  = new ArrayList();
-
-            File file = new File("/Users/keeg/workspace/txtFileConverter/src/main/resources/input.txt");
+            File file = new File( args[0] );
             Scanner sc = new Scanner(file);
-            while (sc.hasNextLine()){
-                line = Arrays.asList(sc.nextLine().split(";"));
-                JSONObject obj = new JSONObject();
-                obj.put("numReference", line.get(0));
-                obj.put("type", line.get(1));
-                obj.put("price", line.get(2));
-                obj.put("size", line.get(3));
 
-                if ( !typesList.contains(line.get(1)) || (line.get(0).length() != 10) ){
-                    unhealthyLine.add(obj);
+
+            if (  args[1] != null && args[1].toString().toUpperCase().equals("JSON")){
+
+                JSONObject results = new JSONObject();
+
+                List<JSONObject> healthyLine  = new ArrayList();
+                List<JSONObject> unhealthyLine  = new ArrayList();
+                List<String> line  = new ArrayList();
+
+                while (sc.hasNextLine()){
+                    line = Arrays.asList(sc.nextLine().split(";"));
+                    JSONObject obj = new JSONObject();
+                    obj.put("numReference", line.get(0));
+                    obj.put("type", line.get(1));
+                    obj.put("price", line.get(2));
+                    obj.put("size", line.get(3));
+
+                    if ( !typesList.contains(line.get(1)) || (line.get(0).length() != 10) ){
+                        unhealthyLine.add(obj);
+                    }
+                    else {
+                        healthyLine.add(obj);
+                    }
                 }
-                else {
-                    healthyLine.add(obj);
+
+                results.put("inputFile" ,  args[0]);
+                results.put("references" , healthyLine);
+                results.put("errors" , unhealthyLine);
+
+                try (FileWriter filer = new FileWriter( args[2] )) {
+
+                    filer.write(results.toString());
+                    filer.flush();
+
+                } catch (IOException e) {
+                    e.printStackTrace();
                 }
+
+            } else if (args[1].toString().toUpperCase().equals("XML")){
+               // String xml = XML.toString(results);
+                System.out.println("XML");
             }
 
-            results.put("inputFile" , "EEEEEEEEEEEe");
-            results.put("references" , healthyLine);
-            results.put("errors" , unhealthyLine);
 
 
 
-            try (FileWriter filer = new FileWriter("/Users/keeg/workspace/txtFileConverter/src/main/resources/output.json")) {
 
-                filer.write(results.toString());
-                filer.flush();
 
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
         }
 }
